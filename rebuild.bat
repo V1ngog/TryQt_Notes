e00385659145b4ddea484f5763d1c003c9711b3a
andre <andre@ANDREWPC>
2026-04-22 01:51:05 +0300

    creat first buttuns

---
+++ rebuild.bat
@@ -0,0 +1,12 @@
+@echo off
+echo Destroid ols build...
+if exist build rmdir /s /q build
+
+echo Creating...
+mkdir build
+cd build
+cmake .. -G "MinGW Makefiles" -DCMAKE_PREFIX_PATH=C:/Qt/6.11.0/mingw_64
+cmake --build .
+
+echo Готово!
+pause
