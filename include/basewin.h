337feb95794c0edb7f47faadc152570fe0b8daf8
andre <andre@ANDREWPC>
2026-05-26 18:31:12 +0300

    created virtal class BaseWindow

--- include/basewin.h
+++ include/basewin.h
@@ -2,10 +2,29 @@
 #define BASEWIN_H
 
 #include <QWidget>
+#include <QVBoxLayout>
 
-class basewin : public QWidget 
+class BaseWindow : public QWidget 
 {
     Q_OBJECT
     
+protected:
+    int const WIN_W = 800;
+    int const WIN_H = 600;
+    QVBoxLayout *mainlayout;
+
+public:
+    explicit BaseWindow(QWidget *parent = nullptr) : QWidget(parent) {
+        mainlayout = new QVBoxLayout(this);
+        setLayout(mainlayout);
+        resize(WIN_W, WIN_H);
+    }
+
+    virtual ~BaseWindow() {}
+
+    virtual void setupUI() = 0;
+    virtual void connectSignals() = 0;
+    virtual void applyStyles() {}
+    virtual void setupShortcuts() {}
 };
 #endif
